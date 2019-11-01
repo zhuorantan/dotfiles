08ab036fe515780719d1fe5632638576757809e7
Zhuoran Tan <zhuoran1997@icloud.com>
2019-11-01 20:48:05 +0800

    fix a typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ DIR=$(pwd)
 ln -snf $DIR/zshrc ~/.zshrc
 ln -snf $DIR/vimrc ~/.vimrc
 ln -snf $DIR/ssh.conf ~/.ssh/config
-ln -snf $DIR/nvim_int.vim ~/.config/nvim/init.vim
+ln -snf $DIR/nvim_init.vim ~/.config/nvim/init.vim
 ln -snf $DIR/tmux.conf ~/.tmux.conf.local
 
 if [[ "$(uname 2> /dev/null)" == "Darwin" ]]; then
