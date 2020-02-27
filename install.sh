46e90addf76e91aff404b57b1d553a74c49a8f18
Zhuoran Tan <zhuoran1997@icloud.com>
2020-02-27 16:46:13 +0800

    remove some configs

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 DIR=$(pwd)
 ln -snf $DIR/zshrc ~/.zshrc
 ln -snf $DIR/vimrc ~/.vimrc
-ln -snf $DIR/ssh.conf ~/.ssh/config
 ln -snf $DIR/nvim_init.vim ~/.config/nvim/init.vim
 ln -snf $DIR/tmux.conf ~/.tmux.conf.local
 
-if [[ "$(uname 2> /dev/null)" == "Darwin" ]]; then
-    ln -snf $DIR/clash_config.yaml ~/.config/clash/config.yaml
-fi
