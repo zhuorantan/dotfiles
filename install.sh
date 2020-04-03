0ce64cf611bef10019f1162b1573d1ece8351d98
Zhuoran Tan <zhuoran1997@icloud.com>
2020-04-03 20:24:44 +0800

    feat: add more tmux options

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ DIR=$(pwd)
 ln -snf $DIR/zshrc ~/.zshrc
 ln -snf $DIR/vimrc ~/.vimrc
 ln -snf $DIR/nvim_init.vim ~/.config/nvim/init.vim
-ln -snf $DIR/tmux.conf ~/.tmux.conf.local
+ln -snf $DIR/tmux.conf ~/.tmux.conf
 
