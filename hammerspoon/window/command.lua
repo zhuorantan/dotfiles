01f8431bfccd727e2fc6f7d59a70944a7d552b0c
Zhuoran Tan <zhuoran.tan@microsoft.com>
2021-08-05 19:33:56 +0800

    More command defs

--- hammerspoon/window/command.lua
+++ hammerspoon/window/command.lua
@@ -3,27 +3,63 @@ local Validate = require('window/validate')
 
 local command = {}
 
-local moveMap = {
+local moveDefinitions = {
     moveUp = {
-        {
+        map = {
             bottomHalf = "topHalf",
             bottomThird = "centerHorizontalThird",
             bottomTwoThirds = "topTwoThirds",
             centerHorizontalThird = "topThird",
         },
-        "topHalf"
+        default = "topHalf",
     },
+    moveDown = {
+        map = {
+            topHalf = "bottomHalf",
+            topThird = "centerHorizontalThird",
+            topTwoThirds = "bottomTwoThirds",
+            centerHorizontalThird = "bottomThird",
+        },
+        default = "bottomHalf",
+    },
+    moveLeft = {
+        map = {
+            rightHalf = "leftHalf",
+            rightThird = "centerVerticalThird",
+            rightTwoThirds = "leftTwoThirds",
+            centerVerticalThird = "leftThird",
+        },
+        default = "leftHalf",
+    },
+    moveRight = {
+        map = {
+            leftHalf = "rightHalf",
+            leftThird = "centerVerticalThird",
+            leftTwoThirds = "rightTwoThirds",
+            centerVerticalThird = "rightThird",
+        },
+        default = "rightHalf",
+    },
+    resizeUp = {
+        map = {
+            fullScreen = "topTwoThirds",
+            topHalf = "topThird",
+            topTwoThirds = "topHalf",
+            bottomHalf = "bottomTwoThirds",
+            bottomThird = "bottomHalf",
+        }
+    }
 }
 
-for name, map in pairs(moveMap) do
+for name, definition in pairs(moveDefinitions) do
     command[name] = function (window, screen)
-        for current, target in pairs(map[1]) do
+        for current, target in pairs(definition.map) do
             if Validate[current](window, screen) then
                 return Resize[target](window, screen)
             end
         end
 
-        return Resize[map[2]](window, screen)
+        return Resize[definition.default](window, screen)
     end
 end
 
@@ -33,48 +69,6 @@ function command.moveCenter(window, screen)
     else
         return Resize.verticalCenter(window, screen)
     end
-end
-
-function command.moveDown(window, screen)
-    if Validate.topHalf(window, screen) then
-        return Resize.bottomHalf(window, screen)
-    elseif Validate.topThird(window, screen) then
-        return Resize.centerHorizontalThird(window, screen)
-    elseif Validate.topTwoThirds(window, screen) then
-        return Resize.bottomTwoThirds(window, screen)
-    elseif Validate.centerHorizontalThird(window, screen) then
-        return Resize.bottomThird(window, screen)
-    end
-
-    return Resize.bottomHalf(window, screen)
-end
-
-function command.moveLeft(window, screen)
-    if Validate.rightHalf(window, screen) then
-        return Resize.leftHalf(window, screen)
-    elseif Validate.rightThird(window, screen) then
-        return Resize.centerVerticalThird(window, screen)
-    elseif Validate.rightTwoThirds(window, screen) then
-        return Resize.leftTwoThirds(window, screen)
-    elseif Validate.centerVerticalThird(window, screen) then
-        return Resize.leftThird(window, screen)
-    end
-
-    return Resize.leftHalf(window, screen)
-end
-
-function command.moveRight(window, screen)
-    if Validate.leftHalf(window, screen) then
-        return Resize.rightHalf(window, screen)
-    elseif Validate.leftThird(window, screen) then
-        return Resize.centerVerticalThird(window, screen)
-    elseif Validate.leftTwoThirds(window, screen) then
-        return Resize.rightTwoThirds(window, screen)
-    elseif Validate.centerVerticalThird(window, screen) then
-        return Resize.rightThird(window, screen)
-    end
-
-    return Resize.rightHalf(window, screen)
 end
 
 function command.resizeUp(window, screen)
