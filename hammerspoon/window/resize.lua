b17a0304d56cd6fd1006cc1f139418ca4d11ae10
Zhuoran Tan <zhuoran.tan@microsoft.com>
2021-08-05 18:49:24 +0800

    Refactor resize

--- hammerspoon/window/resize.lua
+++ hammerspoon/window/resize.lua
@@ -1,140 +1,36 @@
-local resize = {}
+local function sizeFunc(units)
+    return function (window, screen)
+        window.x = screen.w * units.x + screen.x
+        window.y = screen.h * units.y + screen.y
+        window.w = screen.w * units.w
+        window.h = screen.h * units.h
 
-function resize.fullScreen(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w
-    window.h = screen.h
-    return window
+        return window
+    end
 end
 
-function resize.horizontalCenter(window, screen)
-    window.x = screen.w // 4 + screen.x
-    window.y = screen.h // 8 + screen.y
-    window.w = screen.w // 2
-    window.h = screen.h * 3 // 4
-    return window
-end
 
-function resize.verticalCenter(window, screen)
-    window.x = screen.w // 8 + screen.x
-    window.y = screen.h // 4 + screen.y
-    window.w = screen.w * 3 // 4
-    window.h = screen.h // 2
-    return window
-end
+return {
+    fullScreen = sizeFunc({x=0, y=0, w=1, h=1}),
+    horizontalCenter = sizeFunc({x=1/4, y=1/8, w=1/2, h=3/4}),
+    verticalCenter = sizeFunc({x=1/8, y=1/4, w=3/4, h=1/2}),
 
-function resize.leftHalf(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w // 2
-    window.h = screen.h
-    return window
-end
+    leftHalf = sizeFunc({x=0, y=0, w=1/2, h=1}),
+    leftThird = sizeFunc({x=0, y=0, w=1/3, h=1}),
+    leftTwoThirds= sizeFunc({x=0, y=0, w=2/3, h=1}),
 
-function resize.leftThird(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w // 3
-    window.h = screen.h
-    return window
-end
+    bottomHalf = sizeFunc({x=0, y=1/2, w=1, h=1/2}),
+    bottomThird = sizeFunc({x=0, y=2/3, w=1, h=1/3}),
+    bottomTwoThirds = sizeFunc({x=0, y=1/3, w=1, h=2/3}),
 
-function resize.leftTwoThirds(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w // 3 * 2
-    window.h = screen.h
-    return window
-end
+    topHalf = sizeFunc({x=0, y=0, w=1, h=1/2}),
+    topThird = sizeFunc({x=0, y=0, w=1, h=1/3}),
+    topTwoThirds = sizeFunc({x=0, y=0, w=1, h=2/3}),
 
-function resize.bottomHalf(window, screen)
-    window.x = screen.x
-    window.y = screen.h // 2 + screen.y
-    window.w = screen.w
-    window.h = screen.h // 2
-    return window
-end
+    rightHalf = sizeFunc({x=1/2, y=0, w=1/2, h=1}),
+    rightThird = sizeFunc({x=2/3, y=0, w=1/3, h=1}),
+    rightTwoThirds = sizeFunc({x=1/3, y=0, w=2/3, h=1}),
 
-function resize.bottomThird(window, screen)
-    window.x = screen.x
-    window.y = screen.h // 3 * 2 + screen.y
-    window.w = screen.w
-    window.h = screen.h // 3
-    return window
-end
-
-function resize.bottomTwoThirds(window, screen)
-    window.x = screen.x
-    window.y = screen.h // 3 + screen.y
-    window.w = screen.w
-    window.h = screen.h // 3 * 2
-    return window
-end
-
-function resize.topHalf(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w
-    window.h = screen.h // 2
-
-    return window
-end
-
-function resize.topThird(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w
-    window.h = screen.h // 3
-    return window
-end
-
-function resize.topTwoThirds(window, screen)
-    window.x = screen.x
-    window.y = screen.y
-    window.w = screen.w
-    window.h = screen.h // 3 * 2
-    return window
-end
-
-function resize.rightHalf(window, screen)
-    window.x = screen.w // 2 + screen.x
-    window.y = screen.y
-    window.w = screen.w // 2
-    window.h = screen.h
-    return window
-end
-
-function resize.rightThird(window, screen)
-    window.x = screen.w // 3 * 2 + screen.x
-    window.y = screen.y
-    window.w = screen.w // 3
-    window.h = screen.h
-    return window
-end
-
-function resize.rightTwoThirds(window, screen)
-    window.x = screen.w // 3 + screen.x
-    window.y = screen.y
-    window.w = screen.w // 3 * 2
-    window.h = screen.h
-    return window
-end
-
-function resize.centerHorizontalThird(window, screen)
-    window.x = screen.x
-    window.y = screen.h // 3
-    window.w = screen.w
-    window.h = screen.h // 3
-    return window
-end
-
-function resize.centerVerticalThird(window, screen)
-    window.x = screen.w // 3
-    window.y = screen.y
-    window.w = screen.w // 3
-    window.h = screen.h
-    return window
-end
-
-return resize
+    centerHorizontalThird = sizeFunc({x=0, y=1/3, w=1, h=1/3}),
+    centerVerticalThird = sizeFunc({x=1/3, y=0, w=1/3, h=1}),
+}
