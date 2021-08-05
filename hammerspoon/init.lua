e0d3d1cb70cb2782fb0b0ae49f0cf914a10cfede
Zhuoran Tan <zhuoran.tan@microsoft.com>
2021-08-05 16:16:06 +0800

    Distinguish work computer

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,6 @@
 local hyper = {"cmd", "ctrl"}
 local browserAppName = hs.application.get(hs.application.defaultAppForUTI("public.html")):name()
+local isWorkComputer = hs.network.configuration.open():computerName() == "Zhuoran’s MacBook Pro for Work"
 
 -- Fancy config reload
 local function reloadConfig(files)
@@ -37,8 +38,11 @@ hs.hotkey.bind(hyper, "m", toggleApp("Telegram"))
 hs.hotkey.bind(hyper, "u", toggleApp("Messages"))
 hs.hotkey.bind(hyper, "w", toggleApp("WeChat"))
 hs.hotkey.bind(hyper, "0", toggleApp("Music"))
-hs.hotkey.bind(hyper, "o", toggleApp("Microsoft Outlook"))
-hs.hotkey.bind(hyper, "e", toggleApp("Microsoft Teams"))
+
+if isWorkComputer then
+    hs.hotkey.bind(hyper, "o", toggleApp("Microsoft Outlook"))
+    hs.hotkey.bind(hyper, "e", toggleApp("Microsoft Teams"))
+end
 
 -- Enter ScreenSaver
 hs.hotkey.bind(hyper, "8", hs.caffeinate.startScreensaver)
@@ -96,21 +100,30 @@ local appWatcher = hs.application.watcher.new(toggleEmacsNaviKeys):start()
 local leftScreen = hs.screen{x=0,y=0}
 local rightScreen = hs.screen{x=1,y=0}
 
-local windowLayout = {
-    {"Terminal", nil, leftScreen, hs.layout.maximized, nil, nil},
-    {"Xcode", nil, rightScreen, hs.layout.maximized, nil, nil},
-    {"Simulator", nil, leftScreen, hs.layout.right25, nil, nil},
-    {browserAppName, nil, leftScreen, hs.layout.left75, nil, nil},
+local windowLayout
 
-    {"Telegram", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
-    {"Messages", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
-    {"WeChat", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
+if isWorkComputer then
+    windowLayout = {
+        {"Terminal", nil, leftScreen, hs.layout.maximized, nil, nil},
+        {"Xcode", nil, rightScreen, hs.layout.maximized, nil, nil},
+        {"Simulator", nil, leftScreen, hs.layout.right25, nil, nil},
+        {browserAppName, nil, leftScreen, hs.layout.left75, nil, nil},
 
-    {"Music", nil, leftScreen, {x=0.05, y=0.1, w=0.55, h=0.8}, nil, nil},
+        {"Telegram", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
+        {"Messages", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
+        {"WeChat", nil, leftScreen, {x=0.5, y=0.15, w=0.45, h=0.7}, nil, nil},
 
-    {"Microsoft Outlook", nil, leftScreen, {x=0.15, y=0.05, w=0.7, h=0.9}, nil, nil},
-    {"Microsoft Teams", nil, leftScreen, {x=0.15, y=0.05, w=0.7, h=0.9}, nil, nil},
-}
+        {"Music", nil, leftScreen, {x=0.05, y=0.1, w=0.55, h=0.8}, nil, nil},
+
+        {"Microsoft Outlook", nil, leftScreen, {x=0.15, y=0.05, w=0.7, h=0.9}, nil, nil},
+        {"Microsoft Teams", nil, leftScreen, {x=0.15, y=0.05, w=0.7, h=0.9}, nil, nil},
+    }
+else
+    windowLayout = {
+        {"Terminal", nil, rightScreen, hs.layout.maximized, nil, nil},
+        {"Xcode", nil, rightScreen, hs.layout.maximized, nil, nil},
+    }
+end
 
 hs.layout.apply(windowLayout)
 hs.hotkey.bind(hyper, "'", function()
