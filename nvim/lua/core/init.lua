dd1e174f349e7b0478a52c9fe7476944d86a27bc
Zhuoran Tan <zhuoran.tan@microsoft.com>
2021-10-18 21:16:04 +0800

    Stat migrating vimrc to lua

---
+++ nvim/lua/core/init.lua
@@ -0,0 +1,45 @@
+local global = require('core.global')
+local vim = vim
+
+local function disable_distribution_plugins()
+  vim.g.loaded_gzip = 1
+  vim.g.loaded_tar = 1
+  vim.g.loaded_tarPlugin = 1
+  vim.g.loaded_zip = 1
+  vim.g.loaded_zipPlugin = 1
+  vim.g.loaded_getscript = 1
+  vim.g.loaded_getscriptPlugin = 1
+  vim.g.loaded_vimball = 1
+  vim.g.loaded_vimballPlugin = 1
+  vim.g.loaded_matchit = 1
+  vim.g.loaded_matchparen = 1
+  vim.g.loaded_2html_plugin = 1
+  vim.g.loaded_logiPat = 1
+  vim.g.loaded_rrhelper = 1
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+  vim.g.loaded_netrwSettings = 1
+  vim.g.loaded_netrwFileHandlers = 1
+end
+
+local function map_leader()
+  vim.g.mapleader = " "
+  vim.api.nvim_set_keymap('n', ' ', '', { noremap = true })
+  vim.api.nvim_set_keymap('x', ' ', '', { noremap = true })
+end
+
+local function load_core()
+  local pack = require('core.pack')
+
+  disable_distribution_plugins()
+  leader_map()
+
+  -- pack.ensure_plugins()
+  require('core.options')
+  -- require('core.mapping')
+  -- require('keymap')
+  -- require('core.event')
+  -- pack.load_compile()
+end
+
+load_core()
