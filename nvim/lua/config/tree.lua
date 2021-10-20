bf19af274388bf853364c67340afe13a97706bdf
Zhuoran Tan <t@zhuorantan.com>
2021-10-20 21:06:23 +0800

    Update tree

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -1,8 +1,15 @@
+local vim = vim
+
 local config = {}
 
 function config.before()
   local bind = require('utils.bind')
+
   bind.nmap_cmd('<leader>n', 'NvimTreeToggle')
+
+  vim.g.nvim_tree_ignore = { '.git' }
+  vim.g.nvim_tree_quit_on_open = 1
+  vim.g.nvim_tree_indent_markers = 1
 end
 
 function config.after()
@@ -10,6 +17,9 @@ function config.after()
   nvim_tree.setup({
     auto_close = true,
     update_focused_file = {
+      enable = true,
+    },
+    diagnostics = {
       enable = true,
     },
   })
