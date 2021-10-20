9903aba51e930788484b395f5d12126aa3bbd200
Zhuoran Tan <t@zhuorantan.com>
2021-10-20 20:35:23 +0800

    Enable true color

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -3,6 +3,10 @@ local vim = vim
 local config = {}
 
 function config.after()
+  if os.getenv('TMUX') and vim.fn.has('termguicolors') then
+    vim.o.termguicolors = true
+  end
+
   vim.cmd('colorscheme onedark')
 end
 
