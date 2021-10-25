9aea33f60709705240f86d05515d2877b9d8f921
Zhuoran Tan <zhuoran.tan@microsoft.com>
2021-10-25 20:32:56 +0800

    Exclude nofile for indent blankline

--- nvim/lua/config/indent-blankline.lua
+++ nvim/lua/config/indent-blankline.lua
@@ -4,7 +4,7 @@ function M.after()
   local indent_blankline = require('indent_blankline')
 
   indent_blankline.setup({
-    buftype_exclude = { 'terminal' },
+    buftype_exclude = { 'terminal', 'nofile' },
   })
 end
 
