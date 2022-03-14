40c84f836bdb59d0b092840baae6921ba00bb350
Zhuoran Tan <t@zhuorantan.com>
2022-03-14 14:17:13 +0800

    Add rust

--- nvim/lua/config/lsp/servers.lua
+++ nvim/lua/config/lsp/servers.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.servers = { 'clangd', 'cmake', 'cssls', 'dockerls', 'eslint', 'html', 'jsonls', 'sumneko_lua', 'solargraph', 'sourcekit', 'pyright', 'tsserver' }
+M.servers = { 'clangd', 'cmake', 'cssls', 'dockerls', 'eslint', 'html', 'jsonls', 'sumneko_lua', 'solargraph', 'sourcekit', 'pyright', 'tsserver', 'rust_analyzer' }
 M.configs = {}
 
 return M
