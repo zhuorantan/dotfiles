4d0df5d7650623f96db838935361c7ea900cc97b
Zhuoran Tan <t@zhuorantan.com>
2022-04-10 20:34:57 +0800

    Rename refile

--- nvim/lua/config/orgmode.lua
+++ nvim/lua/config/orgmode.lua
@@ -18,7 +18,7 @@ function M.after()
 
   orgmode.setup({
     org_agenda_files = {'~/Documents/org/*'},
-    org_default_notes_file = '~/Documents/org/inbox.org',
+    org_default_notes_file = '~/Documents/org/refile.org',
   })
 end
 
