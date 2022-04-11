5d283e1a42df38d38c98fc6d786bba2d3c69526d
Zhuoran Tan <zhuoran.tan@microsoft.com>
2022-04-11 14:17:18 +0800

    Fix nvim tree deprecation

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -6,13 +6,16 @@ function config.before()
   local bind = require('utils.bind')
 
   bind.nmap_cmd('<leader>n', 'NvimTreeFindFileToggle')
-
-  vim.g.nvim_tree_indent_markers = 1
 end
 
 function config.after()
   local nvim_tree = require('nvim-tree')
   nvim_tree.setup({
+    renderer = {
+      indent_markers = {
+        enable = true,
+      },
+    },
     update_focused_file = {
       enable = true,
     },
