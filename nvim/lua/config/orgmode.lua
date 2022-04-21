c677981d1c883e5fd26e41464d723d8d124c4426
Zhuoran Tan <zhuoran.tan@microsoft.com>
2022-04-21 18:25:59 +0800

    Fix orgmode treesitter

--- nvim/lua/config/orgmode.lua
+++ nvim/lua/config/orgmode.lua
@@ -4,6 +4,18 @@ function M.after()
   local orgmode = require('orgmode')
 
   orgmode.setup_ts_grammar()
+
+  local treesitter = require('nvim-treesitter.configs')
+  treesitter.setup({
+    -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+    highlight = {
+      enable = true,
+      disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+      additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+    },
+    ensure_installed = {'org'}, -- Or run :TSUpdate org
+  })
+
   orgmode.setup({
     org_agenda_files = {'~/Documents/org/*'},
     org_default_notes_file = '~/Documents/org/refile.org',
