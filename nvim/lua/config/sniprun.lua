9c261140d3810a031f255a12b3a555bd10f3a517
Zhuoran Tan <t@zhuorantan.com>
2022-04-28 14:56:23 +0800

    Run snip for a file

--- nvim/lua/config/sniprun.lua
+++ nvim/lua/config/sniprun.lua
@@ -5,6 +5,7 @@ local M = {}
 function M.before()
   vim.keymap.set('n', '<leader>rr', function() require('sniprun').run() end)
   vim.keymap.set('v', '<leader>rr', function() require('sniprun').run('v') end)
+  vim.keymap.set('n', '<leader>rR', function() require('sniprun').run('w') end)
 end
 
 function M.after()
