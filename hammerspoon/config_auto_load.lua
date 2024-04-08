639b10df935e73fe3b3412652642b093a103314d
Zhuoran Tan <t@zhuorantan.com>
2024-04-08 17:19:55 +0800

    Optimize hammerspoon config

--- hammerspoon/config_auto_load.lua
+++ hammerspoon/config_auto_load.lua
@@ -2,27 +2,17 @@ local hs = hs
 
 -- Fancy config reload
 local function reloadConfig(files)
-  local doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
+	local doReload = false
+	for _, file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
 
-  if doReload then
-    hs.reload()
-  end
+	if doReload then
+		hs.reload()
+	end
 end
 
 ConfigWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Documents/dotfiles/hammerspoon", reloadConfig):start()
 hs.alert.show("Config loaded")
-
-local numberOfScreens = #hs.screen.allScreens()
-
-ScreenWatcher = hs.screen.watcher.new(function()
-  if #hs.screen.allScreens() == numberOfScreens then
-    return
-  end
-  numberOfScreens = #hs.screen.allScreens()
-  hs.reload()
-end):start()
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,6 @@ local hs = hs
 -- config
 hs.window.animationDuration = 0
 
-require('config_auto_load')
-require('config_display_sleep')
-require('keybindings').set_up()
-require('layouts')()
+require("config_auto_load")
+require("config_display_sleep")
+require("keybindings").set_up()
