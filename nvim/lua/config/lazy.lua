2d50ffde840e1952a00bb87ee86f503c5433c638
Zhuoran Tan <me@zhuorant.com>
2024-11-12 19:48:26 -0800

    fix for new LazyVim version

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
       },
     },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.dial" },
