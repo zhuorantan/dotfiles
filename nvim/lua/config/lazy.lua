d6efb49923a3a4f22c757ba2e844173327f2e090
Zhuoran Tan <me@zhuorant.com>
2024-11-27 12:59:10 -0800

    use mini-surround

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require("lazy").setup({
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ai.codeium" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.dial" },
