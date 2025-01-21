252902808aa97f7a4b61c035d224c0613b115b66
Zhuoran Tan <me@zhuorant.com>
2025-01-21 11:08:38 -0800

    remove protobuf files from clangd lsp

--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -3,4 +3,14 @@ return {
     "williamboman/mason.nvim",
     opts = { ensure_installed = { "buf" } }, -- protobuf
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+        },
+      },
+    },
+  },
 }
