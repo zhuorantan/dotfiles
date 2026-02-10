85c7adf98b2250a6800d5d60764070609625e9bc
Zhuoran Tan <me@zhuorant.com>
2026-02-10 11:05:06 -0800

    disable markdown formatting and linting

--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -1,7 +1,33 @@
+local markdownlint_config = vim.fn.stdpath("config") .. "/.markdownlint.yaml"
+
 return {
   {
     "mason-org/mason.nvim",
     opts = { ensure_installed = { "buf" } }, -- protobuf
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs({ "markdown", "markdown.mdx" }) do
+        local formatters = opts.formatters_by_ft[ft] or {}
+        opts.formatters_by_ft[ft] = vim.tbl_filter(function(formatter)
+          return formatter ~= "prettier"
+        end, formatters)
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", markdownlint_config, "-" },
+        },
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
